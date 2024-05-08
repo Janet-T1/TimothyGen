523e35e27156a665da99f2f76409bc630f7302cc
jt-qwerty <janet.tsegazeab@gmail.com>
2024-05-08 13:14:56 -0600

    my changes

--- timothy-gen/src/app/page.js
+++ timothy-gen/src/app/page.js
@@ -4,7 +4,10 @@ import Cards from './cards/page.js'
 export default function Home() {
   return (
     <main  className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Cards/>
+     
+    
+    <Cards/>
+  
     </main>
   );
 }
